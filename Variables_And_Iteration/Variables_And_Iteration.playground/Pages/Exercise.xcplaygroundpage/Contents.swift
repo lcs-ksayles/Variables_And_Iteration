844f4ba19028495c4f3aa94f741e4d69e747a832
Sayles Keegan <ksayles@lcs-mba3139.lcs.on.ca>
2017-10-10 11:22:44 -0400

    Made progress on mountain scene

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -16,6 +16,26 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
+//Draw circles
+for x in stride(from: 0, through: 300, by: 30) {
+    for y in stride(from: 0, to: 300, by: 30) {
+canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
+    }
+}
+//Draw large mountains
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 70, alpha: 100)
+canvas.drawRectangle(centreX: 150, centreY: 80, width: 300, height: 160)
+
+//Draw smaller mountains
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 40, alpha: 100)
+canvas.drawRectangle(centreX: 150, centreY: 30, width: 300, height: 110)
+
+//Draw mountain tops
+for i in stride(from: 0, through: 150, by: 1) {
+canvas.drawLine(fromX: i, fromY: 100, toX: 50 + i, toY: 200 - i)
+}
 //: ## Add your code below
 
 
