2a41eb37d5e8732d6db9f01b5d605ebbaf99b114
Sayles Keegan <ksayles@lcs-mba3139.lcs.on.ca>
2017-10-17 11:23:40 -0400

    Lines

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -20,19 +20,29 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Draw lines
+// Draw Lines
 for x in stride(from: 0, through: 500, by: 10)  {
-
-    canvas.drawLine(fromX: 500, fromY: x, toX: 0, toY: 500)
-}
-
-// Draw lines
-for y in stride(from: 500, through: 0, by: -10)  {
     
-    canvas.drawLine(fromX: y, fromY: 500, toX: 500, toY: 0)
+    canvas.drawLine(fromX: 250, fromY: 500, toX: x, toY:0)
 }
 
+// Draw Lines
+for y in stride(from: 0, through: 500, by: 10)  {
+    
+    canvas.drawLine(fromX: 500, fromY: 250, toX: 0, toY: y)
+}
 
+// Draw Lines
+for z in stride(from: 0, through: 500, by: 10)  {
+    
+    canvas.drawLine(fromX: 250, fromY: 0, toX: z, toY: 500)
+}
+
+// Draw Lines
+for i in stride(from: 0, through: 500, by: 10)  {
+    
+    canvas.drawLine(fromX: 0, fromY: 250, toX: 500, toY: i)
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
