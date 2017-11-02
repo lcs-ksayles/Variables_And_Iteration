329bd3ab9387c59f634d174a61c80b37941da8a2
Sayles Keegan <ksayles@lcs-mba3139.lcs.on.ca>
2017-11-02 14:36:03 -0400

    Finished design

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -66,18 +66,7 @@ for i in stride(from: 0, through: 500, by: 10)  {
     canvas.drawLine(fromX: 0, fromY: 250, toX: 500, toY: i)
 }
 
-// Write Name
-canvas.lineColor = Color.red
-canvas.drawLine(fromX: 400, fromY: 0, toX: 400, toY: 30)
-canvas.drawLine(fromX: 400, fromY: 15, toX: 410, toY: 30)
-canvas.drawLine(fromX: 400, fromY: 15, toX: 410, toY: 0)
 
-canvas.drawLine(fromX: 415, fromY: 0, toX: 415, toY: 30)
-canvas.drawLine(fromX: 415, fromY: 1, toX: 430, toY: 2)
-canvas.drawLine(fromX: 415, fromY: 15, toX: 430, toY: 16)
-canvas.drawLine(fromX: 415, fromY: 30, toX: 430, toY: 31)
-
-canvas.drawLine(fromX: 440, fromY: 0, toX: 440, toY: 15)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
